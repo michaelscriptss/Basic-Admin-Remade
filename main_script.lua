e4e129e5c8bfc32ea3c62736e960cfae9ec39a5b
Ethan <umotion18@gmail.com>
2026-08-07 20:13:34 +0500

    Update main_script.lua
    Signed-off-by: Ethan <umotion18@gmail.com>

--- main_script.lua
+++ main_script.lua
@@ -73,8 +73,8 @@ local Configuration = {
 	['Kick Reason'] = ('You have been kicked from the server.'), -- Displayed to people that are kicked.
 	['Ban Reason'] = ('You have been banned from the game.'), -- Displayed to people that are banned.
 	['Shutdown Reason'] = ('This server is shutting down..\nTry joining a different server!'), -- Displayed to people when the server is shut down.
-	['Server Message'] = ('BAR Server Message'), -- Displayed in the :sm command's title.
-	['Server Lock Reason'] = ('The server is locked.'), -- Displayed to people being kicked that try to enter a locked server.
+	['Server Message'] = ('Frappuccino Systems'), -- Displayed in the :sm command's title.
+	['Server Lock Reason'] = ('Frappuccino | The server is locked.'), -- Displayed to people being kicked that try to enter a locked server.
 	['Trello'] = false, -- Use trello? HttpService must be enabled.
 	['Trello Board'] = (''), -- Trello board ID.
 	['Trello App Key'] = (''), -- Private trello application key.
@@ -86,7 +86,7 @@ local Configuration = {
 	['System Color'] = Color3.new(0, 0, 0), -- Color of the admin's guis.
 	['Tools Location'] = game.ServerStorage, -- Where the :tools and :give command will get tools from.
 	['Command Confirmation'] = true, -- Validates certain commands like :Ban all, or :PBan all.
-	['Datastore Key'] = ('BAR') -- What cape data, ban data, etc. is stored under. Changing will wipe it.
+	['Datastore Key'] = ('FRAPPUCCINO_ADMIN') -- What cape data, ban data, etc. is stored under. Changing will wipe it.
 	
 }
 
